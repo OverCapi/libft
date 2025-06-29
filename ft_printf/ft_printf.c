ae54d1ac41a64aae3196dc325f3905d9fce844fb
Over_Capi <capi@MacBook-Air-de-Over-Capi.local>
2025-06-29 16:10:50 +0200

    opti ft_printf 1.5s -> 0.3s

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,31 +6,31 @@
 /*   By: capi <capi@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 09:23:40 by llemmel           #+#    #+#             */
-/*   Updated: 2025/06/28 17:16:34 by capi             ###   ########.fr       */
+/*   Updated: 2025/06/29 16:09:39 by capi             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static size_t	ft_converter(va_list ptr, char specifier)
+static size_t	ft_converter(va_list *ptr, char specifier)
 {
 	size_t	len;
 
 	len = 0;
 	if (specifier == 'c')
-		return (char_converter((unsigned char)va_arg(ptr, int)));
+		return (char_converter((unsigned char)va_arg(*ptr, int)));
 	else if (specifier == 's')
-		return (str_converter(va_arg(ptr, char *)));
+		return (str_converter(va_arg(*ptr, char *)));
 	else if (specifier == 'p')
-		return (ptr_converter((size_t)va_arg(ptr, void *)));
+		return (ptr_converter((size_t)va_arg(*ptr, void *)));
 	else if (specifier == 'd' || specifier == 'i')
-		return (dec_converter(va_arg(ptr, int), 1));
+		return (dec_converter(va_arg(*ptr, int), 1));
 	else if (specifier == 'u')
-		return (dec_converter(va_arg(ptr, unsigned int), 0));
+		return (dec_converter(va_arg(*ptr, unsigned int), 0));
 	else if (specifier == 'x')
-		return (hex_converter(va_arg(ptr, unsigned int), 0));
+		return (hex_converter(va_arg(*ptr, unsigned int), 0));
 	else if (specifier == 'X')
-		return (hex_converter(va_arg(ptr, unsigned int), 1));
+		return (hex_converter(va_arg(*ptr, unsigned int), 1));
 	else if (specifier == '%')
 		return (char_converter('%'));
 	else
@@ -38,7 +38,7 @@ static size_t	ft_converter(va_list ptr, char specifier)
 	return (len);
 }
 
-static int	check_convert(va_list ptr, char specifier, size_t *i, size_t *len)
+static int	check_convert(va_list *ptr, char specifier, char ** format, size_t *len)
 {
 	if (!specifier)
 		return (-1);
@@ -47,78 +47,48 @@ static int	check_convert(va_list ptr, char specifier, size_t *i, size_t *len)
 		ft_putchar_fd('%', 1);
 		ft_putchar_fd(specifier, 1);
 		*len += 2;
-		*i += 1;
+		(*format)++;
 	}
 	else
 	{
 		*len += ft_converter(ptr, specifier);
-		*i += 1;
+		(*format)++;
 	}
 	return (0);
 }
 
 int	ft_printf(const char *format, ...)
 {
-	size_t	i;
 	size_t	len;
+	size_t	write_size;
+	char	*pos;
 	va_list	ptr;
 
-	i = 0;
-	len = 0;
-	va_start(ptr, format);
 	if (!format)
 		return (-1);
-	while (format[i] != '\0')
+	va_start(ptr, format);
+	len = 0;
+	while (*format)
 	{
-		if (format[i] == '%')
+		pos = ft_strchr(format, '%');
+		if (pos)
 		{
-			if (check_convert(ptr, format[i + 1], &i, &len) != 0)
-				return (-1);
+			write_size = pos - (format);
+			if (write_size && write(1, format, write_size) == -1)
+				return (va_end(ptr), -1);
+			len += write_size;
+			if (check_convert(&ptr, pos[1], (char **)&format, &len) != 0)
+				return (va_end(ptr), -1);
+			format++;
 		}
 		else
 		{
-			ft_putchar_fd(format[i], 1);
-			len++;
+			ft_putstr_fd((char *)(format), 1);
+			write_size = ft_strlen(format);
+			len += write_size;
 		}
-		i++;
+		format += write_size;
 	}
 	va_end(ptr);
 	return (len);
 }
-
-// int	ft_printf(const char *format, ...)
-// {
-// 	size_t	i;
-// 	size_t	len;
-// 	size_t	write_size;
-// 	char	*pos;
-// 	va_list	ptr;
-
-// 	if (!format)
-// 		return (-1);
-// 	va_start(ptr, format);
-// 	i = 0;
-// 	len = 0;
-// 	while (format[i])
-// 	{
-// 		pos = ft_strchr(format + i, '%');
-// 		if (pos)
-// 		{
-// 			write_size = pos - (format + i);
-// 			if (write_size && write(1, format + i, write_size) == -1)
-// 				return (va_end(ptr), -1);
-// 			len += write_size;
-// 			if (check_convert(ptr, pos[1], &i, &len) != 0)
-// 				return (va_end(ptr), -1);
-// 		}
-// 		else
-// 		{
-// 			ft_putstr_fd((char *)(format + i), 1);
-// 			write_size = ft_strlen(format + i);
-// 			len += write_size;
-// 		}
-// 		i += write_size;
-// 	}
-// 	va_end(ptr);
-// 	return (len);
-// }
